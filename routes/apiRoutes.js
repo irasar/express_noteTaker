ea87686dd4ad8449461e9eaf9c4892358b98339a
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-08 17:02:56 -0400

    office hours help needs to be fixed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,35 @@
-//GET route???
-store.getNote()
-//POST route
+const { response } = require("express");
+const path = require("path");
+const express = require("express");
 
-//DELETE
+const uuid = require("uuid");
+var app = express();
+const store = require("../db/store");
+const notes = require("../db/db.json");
+//GET route
+module.exports = function (app){
+    app.get("/api/notes", function (req, res) {
+store
+.read()
+.then((notes) => response.json(notes))
+.catch((err) => res.status(500).json(err));
+    });
 
-//????
-const db = require("../db/db.json")
-const store = require("../db/store.js")
+// POST route
+app.post("/api/notes", function(req, res){
+    req.body.id = uuid.v1();
+    //passed the data from the request to the class method
+    store
+    .addnote(req.body)
+    .then((note) => response.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+// DELETE route
+app.delete("/api/notes/:id", function(req, res) {
+    store
+    .delete(req.params.id)
+    .then(() => response.json(notes))
+})
+
+
 
